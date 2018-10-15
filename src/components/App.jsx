4907e45c7c203e80ea5682759b733e568b9ef8d6
Husamoa <pawebial@gmail.com>
2018-10-15 17:26:31 +0200

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {HashRouter, Route, Switch} from "react-router-dom";
+
+export default class App extends Component {
+    render() {
+        return (
+                <h1>Hello The Coders!</h1>
+        );
+    }
+}
