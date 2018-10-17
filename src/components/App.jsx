902e3b41158e40d886afbea8037b92f5fba6bd31
Husamoa <pawebial@gmail.com>
2018-10-17 17:21:57 +0200

    first steps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,16 @@
-import React, {Component} from 'react';
-import {HashRouter, Route, Switch} from "react-router-dom";
+import React, { Component, Fragment } from 'react';
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 export default class App extends Component {
     render() {
         return (
-                <h1>Hello The Coders!</h1>
+            <Fragment>
+                <div className="container">
+                    <img src="./images/logo-tsd.jpg"></img>
+                    <h1>Strona w gruntownej przebudowie!</h1>
+                    <p>Najnowsze informacje znajdziesz na naszym funpage'u <a href="https://www.facebook.com/dzwiekowy/">Tenis Stowy Dźwiękowy</a> </p>
+                </div>
+            </Fragment>
         );
     }
 }
