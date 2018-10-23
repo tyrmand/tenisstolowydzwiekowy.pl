a35178bb08a205859226143e31387771772d9184
Husamoa <pawebial@gmail.com>
2018-10-23 20:03:57 +0200

    style correction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,10 +8,10 @@ export default class App extends Component {
         return (
             <Fragment>
                 <Menu />
-                <div className="container">
+                <div className="container d-flex flex-column align-items-center">
                     <img src="./images/logo-tsd.jpg"></img>
-                    <h1>Strona w gruntownej przebudowie!</h1>
-                    <p>Najnowsze informacje znajdziesz na naszym fanpage'u <a href="https://www.facebook.com/dzwiekowy/">Tenis Stołowy Dźwiękowy</a> </p>
+                    <h1 className="h1 text-center">Strona w gruntownej przebudowie!</h1>
+                    <p className="p text-center">Najnowsze informacje znajdziesz na naszym fanpage'u <a href="https://www.facebook.com/dzwiekowy/">Tenis Stołowy Dźwiękowy</a> </p>
                 </div>
             </Fragment>
         );
