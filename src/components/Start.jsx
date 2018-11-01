de865643f65ce14f217829e656ded0c814c6d68e
Husamoa <pawebial@gmail.com>
2018-11-01 08:31:28 +0100

    update start page

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -5,10 +5,15 @@ export default class Start extends Component {
     render() {
         return (
             <Fragment>
-                <div className="container d-flex flex-column align-items-center">
-                    <img src="./images/logo-tsd.jpg"></img>
-                    <h1 className="h1 text-center">Strona w gruntownej przebudowie!</h1>
-                    <p className="p text-center">Najnowsze informacje znajdziesz na naszym fanpage'u <a href="https://www.facebook.com/dzwiekowy/">Tenis Stołowy Dźwiękowy</a> </p>
+                <div className="start-img d-flex justify-content-center">
+                    <div className="container d-flex flex-column justify-content-center align-items-center">
+                        <div className="jumbotron">
+                            <h1 className="display-4">Strona w gruntownej przebudowie!</h1>
+                            <hr className="my-4" />
+                            <p className="lead">Najnowsze informacje znajdziesz na naszym fanpage'u</p>
+                            <a class="btn btn-primary btn-lg" href="https://www.facebook.com/dzwiekowy/" role="button" target="_blank">Tenis Stołowy Dźwiękowy</a>
+                        </div>
+                    </div>
                 </div>
             </Fragment>
         );
