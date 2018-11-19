a61ef27435096925b0165ad381419959d5905419
Michał Surmacz <michals.git@gmail.com>
2018-11-19 17:49:48 +0100

    Adds the new page (Zawody)

---
+++ src/components/History.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Tile from './Tile';
+
+export default class History extends Component {
+    render() {
+        return (
+            <Tile />
+        )
+    }
+}
