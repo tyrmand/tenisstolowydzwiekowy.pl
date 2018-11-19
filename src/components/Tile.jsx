4803fdd02e61c8a65896aa898e0dffdc1333f3b0
Michał Surmacz <michals.git@gmail.com>
2018-11-19 17:50:24 +0100

    Creates a simple tile template

---
+++ src/components/Tile.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class Tile extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Tytuł</h2>
+            </div>
+        )
+    }
+}
